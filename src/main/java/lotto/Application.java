4820a7b0bb0d8ba7b717ac0f9551be6a3c2af6af
함궈 <heoja0123@naver.com>
2022-11-11 20:36:59 +0900

    feat : 로또 구입 금액을 입력받아 로또의 개수를 리턴하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static final int UNIT = 1000;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        recieveAmount();
     }
+
+    public static int recieveAmount(){
+         Integer amount = Integer.parseInt(Console.readLine());
+         return amount / UNIT;
+    }
+
 }
