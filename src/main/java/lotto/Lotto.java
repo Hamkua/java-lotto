195efdfaa4a5aa568b22115d42ac230dbc9e587d
함궈 <heoja0123@naver.com>
2022-11-11 22:30:22 +0900

    feat : Lotto클래스의 생성자에 전달된 리스트에 중복이 존재하는지 검증하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,31 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplication(numbers);
+
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호 리스트의 사이즈가 6이 아닙니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers){
+        Set<Integer> nonduplicateNumbers = Set.copyOf(numbers);
+        if(nonduplicateNumbers.size() != numbers.size()){
+            throw new IllegalArgumentException("로또 번호 리스트에 중복이 존재합니다.");
+        }
+    }
 }
